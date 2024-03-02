85d687a9c3827241e5c0ed97b027ce579995b031
dmz <looooo87@mail.ru>
2024-03-02 11:17:45 +0300

    dsjdsj

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request,template_name= 'blog/index.html')
+
+def  test_fun(request):
+    return render(request,template_name= 'blog/index.html')
